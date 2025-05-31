bd45fea75b8ff96e0175355de7bf8a2beab66433
Ivan <ipkozyrin@gmail.com>
2025-05-31 15:48:12 -0300

    add

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * This is the index file for F OS.
+ * It has to be run from starter
+ */
+
+export { startSystem } from './starters/devLocal/startSystem.js';
+export { System } from './system/System.js';
+export * from './system/context/PackageContext.js';
+export * from './base/AppBase.js';
+export * from './helpers/helpers.js';
+export * from './types/constants.js';
+export * from './types/types.js';
+export * from './uiApi/SquidletAppApiConnection.js';
+export * from './types/Message.js';
