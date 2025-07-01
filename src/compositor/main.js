1f28ece64a0ac6e71ef415f1a86ba7aa5671a814
Ivan <ipkozyrin@gmail.com>
2025-07-01 16:52:38 -0300

    add

--- src/compositor/main.js
+++ src/compositor/main.js
@@ -2,30 +2,52 @@
   const app = document.getElementById('app');
   app.innerHTML = '<div id="root-compositor"></div>';
 
-  const tiles = {
-    top: {},
-    bottom: {},
-    left: {},
-    right: {},
+  const compositorData = {
+    apiBaseUrl: 'http://localhost:3000',
+    uiBaseUrl: 'http://localhost:3000',
+    compositorApiUrl: '/api/v1/compositor',
+    appUiUrl: '/ui',
+    tiles: {
+      top: {
+        height: 48,
+      },
+      bottom: {
+        height: 48,
+      },
+      left: {
+        width: 150,
+      },
+      right: {
+        width: 150,
+      },
+    },
   };
 
   const rootTiles = [];
-  if (tiles.top) {
-    rootTiles.push('<div id="top-tile"></div>');
+  if (compositorData.tiles.top) {
+    rootTiles.push(
+      `<div id="top-tile" style="height: ${compositorData.tiles.top.height}px;"><iframe id="top-iframe" src="${compositorData.uiBaseUrl}${compositorData.appUiUrl}/top-panel.html"></iframe></div>`
+    );
   }
   rootTiles.push('<div id="middle-tile"></div>');
-  if (tiles.bottom) {
-    rootTiles.push('<div id="bottom-tile"></div>');
+  if (compositorData.tiles.bottom) {
+    rootTiles.push(
+      `<div id="bottom-tile" style="height: ${compositorData.tiles.bottom.height}px;"><iframe id="bottom-iframe" src="${compositorData.uiBaseUrl}${compositorData.appUiUrl}/bottom-panel.html"></iframe></div>`
+    );
   }
   document.getElementById('root-compositor').innerHTML = rootTiles.join('\n');
 
   const middleTiles = [];
-  if (tiles.left) {
-    middleTiles.push('<div id="left-tile"></div>');
+  if (compositorData.tiles.left) {
+    middleTiles.push(
+      `<div id="left-tile" style="width: ${compositorData.tiles.left.width}px;"><iframe id="left-iframe" src="${compositorData.uiBaseUrl}${compositorData.appUiUrl}/left-panel.html"></iframe></div>`
+    );
   }
   middleTiles.push('<div id="main-tile"></div>');
-  if (tiles.right) {
-    middleTiles.push('<div id="right-tile"></div>');
+  if (compositorData.tiles.right) {
+    middleTiles.push(
+      `<div id="right-tile" style="width: ${compositorData.tiles.right.width}px;"><iframe id="right-iframe" src="${compositorData.uiBaseUrl}${compositorData.appUiUrl}/right-panel.html"></iframe></div>`
+    );
   }
   document.getElementById('middle-tile').innerHTML = middleTiles.join('\n');
 
