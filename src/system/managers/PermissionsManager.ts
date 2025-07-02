79766fe93e2771f506427a685bffb6a58a9203be
Ivan <ipkozyrin@gmail.com>
2025-07-02 09:49:37 -0300

    change

--- src/system/managers/PermissionsManager.ts
+++ src/system/managers/PermissionsManager.ts
@@ -1,40 +1,18 @@
-import { pathJoin } from 'squidlet-lib';
-import type { FilesIoType } from '../../types/io/FilesIoType.js';
 import type { System } from '../System.js';
-import type { IoBase } from '../base/IoBase.js';
-import {
-  CFG_FILE_EXT,
-  IO_NAMES,
-  ROOT_DIRS,
-  SYNCED_DATA_SUB_DIRS,
-} from '../../types/constants.js';
 
 const SYSTEM_PERMISSIONS_CFG_NAME = 'system.permissions';
 
-// TODO: use configs manager????
-
 export class PermissionsManager {
-  private permissionsPath: string = pathJoin(
-    '/',
-    ROOT_DIRS.syncedData,
-    SYNCED_DATA_SUB_DIRS.configs,
-    `${SYSTEM_PERMISSIONS_CFG_NAME}.${CFG_FILE_EXT}`
-  );
   // object like {entityName: {permissionName: permissionValue}}
   private permissions: Record<string, Record<string, string>> = {};
-
-  private get filesIo(): FilesIoType & IoBase {
-    return this.system.io.getIo<FilesIoType & IoBase>(IO_NAMES.LocalFilesIo);
-  }
 
   constructor(private readonly system: System) {}
 
   async init() {
-    if (await this.filesIo.stat(this.permissionsPath)) {
-      this.permissions = JSON.parse(
-        await this.filesIo.readTextFile(this.permissionsPath)
-      );
-    }
+    this.permissions = await this.system.configs.loadEntityConfig(
+      SYSTEM_PERMISSIONS_CFG_NAME,
+      true
+    );
   }
 
   async savePermissions(
@@ -46,9 +24,10 @@ export class PermissionsManager {
       ...partialPermissions,
     };
 
-    await this.filesIo.writeFile(
-      this.permissionsPath,
-      JSON.stringify(this.permissions)
+    await this.system.configs.saveEntityConfig(
+      SYSTEM_PERMISSIONS_CFG_NAME,
+      this.permissions,
+      true
     );
   }
 
@@ -61,6 +40,10 @@ export class PermissionsManager {
       delete this.permissions[entityName];
     }
 
-    await this.filesIo.rm([this.permissionsPath]);
+    await this.system.configs.saveEntityConfig(
+      SYSTEM_PERMISSIONS_CFG_NAME,
+      this.permissions,
+      true
+    );
   }
 }
