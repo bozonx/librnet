c4d010691fc7611ca6b579f9be4c99bb9b2e9da0
Ivan <ipkozyrin@gmail.com>
2025-07-12 13:18:54 -0300

    add

--- src/packages/SystemCommonPkg/HttpClientDriver/HttpClientDriver.ts
+++ src/packages/SystemCommonPkg/HttpClientDriver/HttpClientDriver.ts
@@ -1,9 +1,1 @@
 import type { DriverIndex } from '../../../types/types.js';
-import type { DriverContext } from '../../../system/context/DriverContext.js';
-import { DriverBase } from '../../../system/base/DriverBase.js';
-
-export const HttpClientDriverIndex: DriverIndex = (ctx: DriverContext) => {
-  return new HttpClientDriver(ctx);
-};
-
-export class HttpClientDriver extends DriverBase {}
